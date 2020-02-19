14aa724ada3da9a17f9176effff8eec583ebaea9
shawn-nyquist <shawn.nyquist@gmail.com>
2020-02-18 19:12:47 -0600

    Created add button and interaction

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-document.getElementById('addBtn').addEventListener('submit', addRest);
-document.getElementById('randomBtn').addEventListener('submit', randomize);
+let optionArr = [];
 
 const addRest = () => {
+    const entry = document.getElementById('restaurantInput').value;
+    const optionList = document.getElementById('optionsDisplay');
+    
+    // Push entry to options array
+    optionArr.push(entry);
+
+    // Clear all displayed options before update
+    optionList.innerHTML = '';
+
+    // iterates through option array to display options with numbering
+    for (let index = 0; index < optionArr.length; index++) {
+        optionList.innerHTML += '<h3>'+ (index + 1) + ': ' + optionArr[index] + '</h3>';        
+    }
+
+    
 
 }
 
+
 const randomize = () => {
-    
+
 }
