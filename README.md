aec78fc755c68fe1ec81fcc0ce007be9cfe6485c
shawn-nyquist <shawn.nyquist@gmail.com>
2020-02-23 19:51:14 -0600

    Cleaned up code. Finished future page

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Restaurant-Picker
 
-## Simple picker application designed for the people who just can't decide where to go for a meal.
+Simple picker application designed for the people who just can't decide where to go for a meal.
 
 Tools Used:
-HTML
-CSS
-JavaScript
-Bootstrap
+- HTML
+- CSS
+- JavaScript
+- Bootstrap
--- main.js
+++ main.js
@@ -3,46 +3,50 @@ const optionList = document.getElementById('optionsDisplay');
 const selection = document.getElementById('displaySelection');
 const enjoyText = document.getElementById('enjoyText');
 
+// Function for adding a restaurant to the array and display
 const addRest = () => {
     const entry = document.getElementById('restaurantInput').value;
     
-    
-    
-    // Push entry to options array
+    // Push entry to options array if not null
     if(entry){
         optionArr.push(entry);
+        optionList.innerHTML = '<h2>Your choices:</h2>';
     } else {
         alert('Please Enter a Restaurant...');
     }
-    
-
-    // Clear all displayed options before update
-    optionList.innerHTML = '<h2>Your choices:</h2>';
-
-
-    // iterates through option array to display options with numbering
-    for (let index = 0; index < optionArr.length; index++) {
-        optionList.innerHTML += '<h3>'+ (index + 1) + ': ' + optionArr[index] + '</h3>';        
-    }
+  
+    displayOptions();
 
     document.getElementById("inputForm").reset();
 
 }
 
-
-const randomize = () => {
-    const randomNum = Math.floor(Math.random() * optionArr.length);
-    
-    
-
-    selection.innerHTML = '<h1>Winner:</h1>' +
-                            '<h4>' + optionArr[randomNum] + '</h4>';
-
-    enjoyText.innerHTML = '<h3>Enjoy your meal!</h3>';
-
-
+// iterates through option array to display options with numbering
+const displayOptions = () => {   
+    for (let index = 0; index < optionArr.length; index++) {
+        optionList.innerHTML += '<h3>'+ (index + 1) + ': ' + optionArr[index] + '</h3>';        
+    }
 }
 
+// Displays the winner
+const displayWinner = () => {
+    const ranNum = ranNumGenerator();
+
+    // Alters selection div with results and displays other text
+    selection.innerHTML = '<h1>Winner:</h1>' +
+                            '<h4>' + optionArr[ranNum] + '</h4>';
+
+    enjoyText.innerHTML = '<h3>Enjoy your meal!</h3>';
+}
+
+// Generates Random Number based on array size
+const ranNumGenerator = () => {
+    const randomNum = Math.floor(Math.random() * optionArr.length);
+
+    return randomNum;
+}
+
+// Clears the form, all arrays, and div sections that were changed
 const clearForm = () => {
     document.getElementById("inputForm").reset();
     optionArr = [];
